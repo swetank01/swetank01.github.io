97e79fc0b2860820923382900ee1507835d7f597
swetank soni <swetankdevops@gmail.com>
2025-07-18 17:46:58 +0000

    this does'not seems like a personal porfolia webiste at all, i need some

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"Space Grotesk"', 'sans-serif'],
-        headline: ['"Space Grotesk"', 'sans-serif'],
+        body: ['"Share Tech Mono"', 'monospace'],
+        headline: ['"Share Tech Mono"', 'monospace'],
         code: ['monospace'],
       },
       colors: {
