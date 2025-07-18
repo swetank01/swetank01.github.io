644d0628e07fd92d07b71e03cff8d8c192a78d3d
swetank soni <swetankdevops@gmail.com>
2025-07-18 18:42:30 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <NeuralNetwork>
    
    R

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -1,20 +1,15 @@
 import { InteractiveTerminal } from '@/components/shared/interactive-terminal';
-import Image from 'next/image';
 import { NeuralNetwork } from '@/components/shared/neural-network';
 
 export function HeroSection() {
   return (
-    <section id="home" className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-      <div className="container px-4 md:px-6">
-        <div className="grid gap-10 lg:grid-cols-2 lg:gap-12 xl:gap-16">
-          <div className="w-full max-w-2xl mx-auto flex items-center">
-            <InteractiveTerminal />
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <div className="w-[400px] h-[400px] lg:w-[500px] lg:h-[500px] relative">
-              <NeuralNetwork />
-            </div>
-          </div>
+    <section id="home" className="relative w-full h-screen flex items-center justify-center overflow-hidden">
+      <div className="absolute inset-0 z-0">
+        <NeuralNetwork />
+      </div>
+      <div className="container z-10 px-4 md:px-6">
+        <div className="w-full max-w-3xl mx-auto">
+          <InteractiveTerminal />
         </div>
       </div>
     </section>
