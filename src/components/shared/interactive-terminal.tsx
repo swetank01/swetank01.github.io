8ff391c437c2095a0e3e03bd0085cb28d27c4f11
swetank soni <swetankdevops@gmail.com>
2025-07-20 22:02:09 +0000

    add this
    "
    KK   KK  AAAAAAAA  LL      SSSSSSS  HH   HH
    KK  KK  AA    A

--- src/components/shared/interactive-terminal.tsx
+++ src/components/shared/interactive-terminal.tsx
@@ -9,12 +9,13 @@ import { GlitchText } from '@/components/shared/glitch-text';
 import { DigitalRain } from '@/components/shared/digital-rain';
 
 const asciiArt = `
-██╗  ██╗ █████╗ ██╗      ██████╗ ██╗  ██╗
-██║ ██╔╝██╔══██╗██║      ██╔══██╗██║  ██║
-█████═╝ ███████║██║      ███████║██╗  ██║
-██╔═██╗ ██╔══██║██║      ██╔═══██╝██║  ██║
-██║  ██╗██║  ██║███████╗  ██████╔╝ ███████║
-╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝  ╚═════╝  ╚══════╝
+KK   KK  AAAAAAAA  LL      SSSSSSS  HH   HH
+KK  KK  AA    AA  LL     SS     SS  HH   HH
+KK KK   AA    AA  LL     SS         HH   HH
+KKKK    AAAAAAAA  LL      SSSSSSS   HHHHHHH
+KK KK   AA    AA  LL           SS   HH   HH
+KK  KK  AA    AA  LL     SS    SS   HH   HH
+KK   KK AA    AA  LLLLLLLL SSSSSSS  HH   HH
 `;
 
 const initialCommands = [
